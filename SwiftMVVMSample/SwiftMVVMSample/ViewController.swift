ac82cd245c7758bd74bdcd13a1a0a52bb1a518b5
Taillook <jousoku@gmail.com>
2017-12-07 18:11:31 +0900

    init

---
+++ SwiftMVVMSample/SwiftMVVMSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMVVMSample
+//
+//  Created by Taillook on 2017/12/07.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
