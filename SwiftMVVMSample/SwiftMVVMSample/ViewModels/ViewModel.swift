b205adf01783b893223160612b53e4aea26baeb2
Taillook <jousoku@gmail.com>
2017-12-08 00:03:33 +0900

    [Add] ViewModel

--- SwiftMVVMSample/SwiftMVVMSample/ViewModels/ViewModel.swift
+++ SwiftMVVMSample/SwiftMVVMSample/ViewModels/ViewModel.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017年 Taillook. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+struct ViewModel {
+    let data = Variable<WeatherObject>(WeatherObject())
+    let request = Requests()
+    
+    init() { }
+    
+    func getWeather(lat: String, lon: String) {
+        request.getWeather(lat: lat, lon: lon) { (data: WeatherObject) in
+            self.data.value = data
+        }
+    }
+}
